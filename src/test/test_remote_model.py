5231344ae239bc910e20971b654d993873af4305
Sandaria117 <dodai11704@gmail.com>
2026-05-30 12:27:50 +0000

    test: add unit test for new remote model

--- src/test/test_remote_model.py
+++ src/test/test_remote_model.py
@@ -3,20 +3,22 @@
 import requests
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
-SASREC_REMOTE_URL = os.getenv("SASREC_REMOTE_URL")
+load_dotenv()
+# SASREC_REMOTE_URL
+REMOTE_MODEL_URL = os.getenv("REMOTE_MODEL_URL")
 
 # Health check
-print(requests.get(f"{SASREC_REMOTE_URL}/health").json())
+print(requests.get(f"{REMOTE_MODEL_URL}/health").json())
 # {"status": "ok", "device": "cuda", "max_len": 200, "num_items": ...}
 
 # Recommend
-res = requests.post(f"{SASREC_REMOTE_URL}/recommend", json={
-    "click_sequence": [1492293,910862,1491172,424964,1515526,440486,109488,1507622,1734061,854637,718983,215311,718983,711125,50049,105393,959544,1734061,1842593,1464360,207905,1628317],
-    "k": 20,
-    "exclude_clicked": True
+r = requests.post(f"{REMOTE_MODEL_URL}/recommend", json={
+    "click_sequence": [1492293, 910862, 1491172,  424964],
+    "type_sequence":  ["clicks", "clicks", "carts", "orders"],
+    "ts_sequence": [1, 2, 3, 4],
+    "model": "lbmrerank",
+    "k": 20
 })
-data = res.json()
-print(data["top_aids"])    
-print(data["top_scores"]) 
+print(r.json()["recommend"])
+print(r.json()["model_used"])
--- src/test/test_sasrec_recommender.py
+++ src/test/test_sasrec_recommender.py
@@ -1,28 +1,24 @@
 #python -m src.test.test_sasrec_recommender
 
-from src.serving.sasrec_recommender import SASRecRecommender
+from src.serving.sasrec_recommender import RemoteModelRecommender
 import os
 from dotenv import load_dotenv
 import asyncio 
 
 load_dotenv()
-SASREC_REMOTE_URL = os.getenv("SASREC_REMOTE_URL")
+REMOTE_MODEL_URL = os.getenv("REMOTE_MODEL_URL")
 
 if __name__ == "__main__":
     async def main():
-        sasrec = SASRecRecommender(remote_url=SASREC_REMOTE_URL)
-        session_aids = [1492293,910862,1491172,424964,1515526,440486,109488]
+        remote_model = RemoteModelRecommender(remote_url=REMOTE_MODEL_URL)
+        click_sequence = [1492293, 910862, 1491172, 424964]
+        type_sequence =   ["clicks", "clicks", "carts", "orders"]# 2 click, 1 cart, 1 buy
+        ts_sequence = [1, 2,3, 4]
+        exclude_clicked = True
+        model = "lbmrerank"
+        k = 5
         print("Test predict (top_aids):")
-        print(await sasrec.predict(session_aids, top_k=20))
-        print("Test recommend_multi_objective:")
-        print(await sasrec.recommend_multi_objective(session_aids, top_k=20))
-        await sasrec.aclose()
+        print(await remote_model.predict_remote(k, click_sequence, type_sequence, ts_sequence, model, exclude_clicked))
+       
+        await remote_model.aclose()
     asyncio.run(main()) 
-
-# {'clicks': [1084785, 1721019, 1781681, 152974, 485465, 165903, 1466188, 1360395, 905594, 730762], 
-#  'carts': [594465, 1353221, 595525, 511946, 819232], 
-#  'orders': [1220971, 1789921, 1812934, 943662, 26203]}
-
-# {'clicks': [1135711, 1471212, 1182024, 1553344, 219758, 1211286, 663062, 1717294, 595525, 1812934], 
-#  'carts': [1074263, 926198, 1005368, 594465, 391942], 
-#  'orders': [517341, 450281, 89935, 1084785, 1220971]}
